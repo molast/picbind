ea2bee4fe13af2485cd4bb2e12fb5a5c038b86f1
chenaimim <loomchen@gmail.com>
2026-04-20 11:45:48 +0800

    fix：修复部署bug

--- web/src/app/robots.ts
+++ web/src/app/robots.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { getSiteUrl } from "@/server/site-config";
 
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
   const siteUrl = getSiteUrl();
 
--- web/src/app/sitemap.ts
+++ web/src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { getSiteUrl } from "@/server/site-config";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = getSiteUrl();
   const now = new Date();
